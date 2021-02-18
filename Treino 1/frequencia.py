2c43b20e45157a62d1c588c58e9b07860bf3b8ac
Tiago Dos Santos Silva Peixoto Carriço <tiagocarrico01@gmail.com>
2021-02-18 21:45:56 +0000

    Update frequencia.py

--- Treino 1/frequencia.py
+++ Treino 1/frequencia.py
@@ -1,4 +1,10 @@
-# frequencia.py
+'''
+Neste problem pretende-se que defina uma função que, dada uma string com palavras, 
+devolva uma lista com as palavras nela contidas ordenada por ordem de frequência,
+da mais alta para a mais baixa. Palavras com a mesma frequência devem ser listadas 
+por ordem alfabética.
+'''
+
 def frequencia(texto):
     palavras = texto.split()
     dicionario = {}
