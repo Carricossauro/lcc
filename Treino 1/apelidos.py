2c395422655287eca0b71336ba6767fdf67cb7ec
Tiago Dos Santos Silva Peixoto Carriço <tiagocarrico27@gmail.com>
2021-02-18 20:57:33 +0000

    Update apelidos.py

--- Treino 1/apelidos.py
+++ Treino 1/apelidos.py
@@ -1,16 +1,4 @@
 # apelidos.py
 def apelidos(nomes):
-    result = []
-    for pessoa in nomes:
-        quantidade = len(pessoa.split())
-        posicao = 0
-        for nome in result:
-            quant = len(nome.split())
-            if quantidade == quant:
-                if nome > pessoa:
-                    break
-            elif quantidade > quant:
-                break
-            posicao += 1
-        result.insert(posicao, pessoa)
-    return result
+    nomes.sort(key=lambda x: (len(x.split()),x))
+    return nomes
