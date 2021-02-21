c52134191b921d623fe2a2195e4e22cf031a860c
Tiago Dos Santos Silva Peixoto Carriço <tiagocarrico01@gmail.com>
2021-02-21 23:07:07 +0000

    Update factoriza.py

--- Treino 1/factoriza.py
+++ Treino 1/factoriza.py
@@ -5,7 +5,7 @@ e produz a soma dos seus factores primos distintos.
 
 def isPrime(x):
     r = True
-    for k in range(2,x//2):
+    for k in range(2,x//2+1):
         if x % k == 0:
             r = False
             break
@@ -25,7 +25,7 @@ def factoriza(n):
 # It's Big Brain Time
 
 def factoriza(n):
-    primos = [x for x in range(2,n+1) if all([x % k != 0 for k in range(2,x//2)])]
+    primos = [x for x in range(2,n+1) if all([x % k != 0 for k in range(2,x//2+1)])]
     result = 0
     for k in primos:
         if (n % k == 0):
@@ -33,4 +33,11 @@ def factoriza(n):
         while (n % k == 0):
             n//=k
 
+    return result
+
+# Even Bigger Brain Time
+
+def factoriza(n):
+    primos = [x for x in range(2,n+1) if all([x % k != 0 for k in range(2,x//2+1)])]
+    result = sum(filter(lambda x: n % x == 0, primos))
     return result
