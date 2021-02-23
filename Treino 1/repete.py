7e2946017c061474e93baabfcb5126aded82dec7
Tiago Dos Santos Silva Peixoto Carriço <tiagocarrico01@gmail.com>
2021-02-23 11:03:00 +0000

    repete mais eficiente

--- Treino 1/repete.py
+++ Treino 1/repete.py
@@ -17,3 +17,17 @@ def repete(palavra,n):
     
     palavra = palavra*n + repeated*(n > 0)
     return palavra
+
+# Mais eficiente (por pouco)
+
+def repete(palavra,n):
+    repeated=""
+    for i in range(1,len(palavra)):
+        el = palavra[i:]
+        if palavra[:len(el)] == el:
+            repeated = el
+            palavra = palavra[:-len(el)]
+            break
+            
+    palavra = palavra*n + repeated*(n > 0)
+    return palavra
