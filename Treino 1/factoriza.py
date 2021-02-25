3cc32e73e0667c501029c3f62707d34790166d36
Tiago Carriço <tiagocarrico01@gmail.com>
2021-02-25 17:28:14 +0000

    100%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

--- Treino 1/factoriza.py
+++ Treino 1/factoriza.py
@@ -3,6 +3,19 @@ Defina uma função que recebe um número positivo
 e produz a soma dos seus factores primos distintos.
 '''
 
+def factoriza(n):
+    soma = 0
+    for k in range(2,n):
+        if n == 1:
+            break
+        if n % k == 0:
+            soma += k
+        while n % k == 0:
+            n/=k
+    return soma
+
+# Para baixo disto não funciona porque faz um loop demasiado grande
+
 def isPrime(x):
     r = True
     for k in range(2,x//2+1):
