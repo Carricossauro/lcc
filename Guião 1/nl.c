2f837f446b4b1cfc569b3a158f466962149bb28b
Tiago Carriço <tiagocarrico27@gmail.com>
2021-03-01 11:49:21 +0000

    Corrections (and nl)

---
+++ Guião 1/nl.c
@@ -0,0 +1,35 @@
+#include<sys/types.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+ssize_t readln(int fd, char *line, size_t size) {
+	char c;
+	size = 0;
+	ssize_t res;
+	do {
+		res = read(fd, &c, 1);
+		if (res == 0) break;
+		line[size++] = c;
+	} while (c != '\n');
+	line[--size] = '\0';
+	return size;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc >= 2) {
+		int numero = 1;
+		char line[100];
+		int fd = open(argv[1], O_RDONLY);
+		ssize_t size;
+		while (1) {
+			if ((size = readln(fd, line, size)) > 0) {
+				write(1, "\t", 1);
+				write(1, line, size);
+				write(1, "\n", 1);
+			} else break;
+
+		}
+	}
+
+	return 0;
+}
