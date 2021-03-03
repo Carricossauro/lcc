f6163d16aecd6c4d66b8ade2dde51339069359c4
Tiago Carriço <tiagocarrico27@gmail.com>
2021-03-03 08:41:42 +0000

    readln improvs

--- Guião 1/mycat.c
+++ Guião 1/mycat.c
@@ -8,11 +8,8 @@
 int main() {
 	ssize_t res;
 	char buffer[MAXBUFFER];
-	while(1) {
-		res = read(0, &buffer, MAXBUFFER);
-		if (res != 0) {
-			res = write(1, &buffer, res);
-		}
+	while((res = read(0, &buffer, MAXBUFFER)) != 0) {
+		res = write(1, &buffer, res);
 	}
 	return 0;
 }
--- Guião 1/readln.c
+++ Guião 1/readln.c
@@ -16,16 +16,18 @@ ssize_t readln(int fd, char *line, size_t size) {
 	return size;
 }
 
-
 /*
+
+#define MAXBUFFER 50
+
 // Ex 4
 ssize_t readln(int fd, char *line, size_t size) {
-	char c[50];
+	char c[MAXBUFFER];
 	size = 0;
 	int i = 0;
 	ssize_t res = 1;
 	while (res) {
-		res = read(fd, &c, 50);
+		res = read(fd, &c, MAXBUFFER);
 		for (i=0; i < res; i++,size++) {
 			if (c[i] == '\n') {
 				line[size++] = '\0';
@@ -39,12 +41,15 @@ ssize_t readln(int fd, char *line, size_t size) {
 }
 */
 
-int main() {
-	char line[100];
+int main(int argc, char *argv[]) {
+	char line[1000];
 	ssize_t size;
-	int fd = open("ola.txt", O_RDONLY);
+	if (argc < 1) return -1;
+	int fd = open(argv[1], O_RDONLY);
 	size = readln(fd, line, size);
 	write(1, &line, size);
+	line[0] = '\n';
+	write(1, line, 1);
 	close(fd);
 	return 0;
 }
