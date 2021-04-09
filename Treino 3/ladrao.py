3e63b0230bcff7d993e7ca275eeab0395f27d2f5
Tiago Carriço <tiagocarrico01@gmail.com>
2021-04-09 11:15:33 +0100

    Update ladrao.py

--- Treino 3/ladrao.py
+++ Treino 3/ladrao.py
@@ -1,3 +1,16 @@
+"""
+
+Um ladrão assalta uma casa e, dado que tem uma capacidade de carga limitada, 
+tem que decidir que objectos vai levar por forma a maximizar o potencial lucro. 
+
+Implemente uma função que ajude o ladrão a decidir o que levar.
+A função recebe a capacidade de carga do ladrão (em Kg) seguida de uma lista 
+dos objectos existentes na casa, sendo cada um um triplo com o nome, o valor de 
+venda no mercado negro, e o seu peso. Deve devolver o máximo lucro que o ladrão
+poderá  obter para a capacidade de carga especificada.
+    
+"""
+
 # Recursiva
 def ladrao(capacidade,objectos):
     if capacidade == 0 or objectos == []:
