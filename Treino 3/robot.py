8b42a5fe998514e017fdf956aca6f31fbd625ad7
Tiago Carriço <tiagocarrico01@gmail.com>
2021-04-15 19:40:14 +0100

    Update robot.py

--- Treino 3/robot.py
+++ Treino 3/robot.py
@@ -10,12 +10,12 @@ O resultado deve ser devolvido com a precisao de 2 casas decimais.
 
 """
 
-# Programação Dinâmica - 10%
+# Programação Dinâmica - 11%
 def probabilidade(passos,probabilidade):
     probs = [{} for x in range(passos+1)]
     
     for k in range(2):
-        for x in range(-passos, passos+1):
+        for x in range(-passos//2-1, passos//2+1):
             for y in range(passos+1):
                 probs[0][(x,y)] = 0.0
             for y in range(-passos,0):
@@ -27,8 +27,8 @@ def probabilidade(passos,probabilidade):
     dy = [ 0,0,1,-1]
 
     for p in range(1, passos+1):
-        for x in range(-passos, passos+1):
-            for y in range(-passos, passos+1):
+        for x in range(-passos//2-1, passos//2+1):
+            for y in range(-passos//2-1, passos//2+1):
                 if (x,y,p) == (0,0,0):
                     probs[p][(x,y)] = 1.0
                 else:
@@ -36,7 +36,7 @@ def probabilidade(passos,probabilidade):
                     for k2 in range(4):
                         X = x + dx[k2]
                         Y = y + dy[k2]
-                        if -passos < X < passos and -passos < Y < passos:
+                        if -passos//2-1 < X < passos//2+1 and -passos//2-1 < Y < passos//2+1:
                             antiga = probabilidade[lado[k2]]*probs[p-1][(X,Y)]
                             probs[p][(x,y)] += antiga
     
