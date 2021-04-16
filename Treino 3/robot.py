e0f3d7f046a78c5443de1d266490b0d9381466cb
Tiago Carriço <tiagocarrico01@gmail.com>
2021-04-16 14:42:11 +0100

    Mudanças de eficiência (ainda não acerta o último caso)

--- Treino 3/robot.py
+++ Treino 3/robot.py
@@ -12,26 +12,29 @@ O resultado deve ser devolvido com a precisao de 2 casas decimais.
 
 # Programação Dinâmica - 11%
 def probabilidade(passos,probabilidade):
-    probs = [{} for x in range(passos+1)]
-
-    for x in range(-passos//2-1, passos//2+1):
-        for y in range(-passos//2-1, passos//2+1):
-            probs[0][(x,y)] = 0.0
+    probs = {}
     
-    probs[0][(0,0)] = 1.0
+    limInf = -passos//3-2
+    limSup = passos//3+2
+
+    for x in range(limInf, limSup):
+        for y in range(limInf, limSup):
+            probs[0,x,y] = 0.0
+    
+    probs[0,0,0] = 1.0
     lado = ['L', 'R', 'U', 'D']
     dx = [-1,1,0, 0]
     dy = [ 0,0,1,-1]
 
     for p in range(1, passos+1):
-        for x in range(-passos//2-1, passos//2+1):
-            for y in range(-passos//2-1, passos//2+1):
-                probs[p][(x,y)] = 0.0
+        for x in range(limInf, limSup):
+            for y in range(limInf, limSup):
+                probs[p,x,y] = 0.0
                 for k2 in range(4):
                     X = x + dx[k2]
                     Y = y + dy[k2]
-                    if -passos//2-1 < X < passos//2+1 and -passos//2-1 < Y < passos//2+1:
-                        antiga = probabilidade[lado[k2]]*probs[p-1][(X,Y)]
-                        probs[p][(x,y)] += antiga
+                    if limInf <= X < limSup and limInf <= Y < limSup:
+                        antiga = probabilidade[lado[k2]]*probs[p-1,X,Y]
+                        probs[p,x,y] += antiga
     
-    return round(probs[passos][(0,0)],2)
+    return round(probs[passos,0,0],2)
