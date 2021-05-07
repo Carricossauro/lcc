3c78cfc5bc07ba48f66945bc5fb9c3eacefa6ae9
Tiago Carriço <tiagocarrico01@gmail.com>
2021-05-07 15:35:11 +0100

    Update sacos.py

--- Treino 4/sacos.py
+++ Treino 4/sacos.py
@@ -9,28 +9,36 @@ todas as compras.
 
 '''
 
-# 10%
 def extensions(peso, compras, k, i, sacos):
     return [x for x in range(k) if sacos[x] + compras[i] <= peso]
 
-def search(peso, compras, N, k, i, sacos):
+def search(peso, compras, N, k, i, sacos, ls):
     if N == i:
         return True
 
+    novoSaco = False
     for x in extensions(peso, compras, k, i, sacos):
+        if sacos[x] == 0:
+            if novoSaco:
+                continue
+            else:
+                novoSaco = True
         sacos[x] += compras[i]
-        if search(peso, compras, N, k, i+1, sacos):
+        ls[i] = x
+        if search(peso, compras, N, k, i+1, sacos, ls):
             return True
+        ls[i] = -1
         sacos[x] -= compras[i]
 
     return False
 
 def sacos(peso,compras):
     N = len(compras)
-
+    print(peso, compras)
     for k in range(1, len(compras)):
         sacos = [0 for _ in range(k)]
-        if search(peso, compras, N, k, 0, sacos):
+        ls = [-1 for _ in range(N)]
+        if search(peso, compras, N, k, 0, sacos, ls):
             return k
 
     return N
