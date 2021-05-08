b074e1eb36b51f06884f42a71f10fae97fdd4fa9
Tiago Carriço <tiagocarrico01@gmail.com>
2021-05-08 13:06:28 +0100

    Update cobertura.py

--- Treino 4/cobertura.py
+++ Treino 4/cobertura.py
@@ -1,52 +1,46 @@
 '''
 
-Implemente uma função que calcula o número mínimo de nós de um grafo 
-não orientado que cobrem todas as arestas, ou seja, o tamanho do menor 
-conjunto de nós que contém pelo menos um extremo de cada aresta. 
-A função recebe a lista de todas as arestas do grafo, sendo cada aresta um 
-par de nós.
+Implemente uma função que descubra o maior conjunto de pessoas que se conhece
+mutuamente. A função recebe receber uma sequências de pares de pessoas que se
+conhecem e deverá devolver o tamanho do maior conjunto de pessoas em que todos
+conhecem todos os outros.
 
 '''
 
 # 10%
-def extensions(ls, k, total, n):
-    return [x for x in range(k, total) if x not in ls and total - x + k >= n]
+def existeAresta(adj, a, b, amigos):
+    return amigos[a] in adj[amigos[b]]
 
-def valid(ls, n, arestas, incluido):
-    return all(map(lambda tup: incluido[tup[0]] or incluido[tup[1]], arestas))
+def extensions(n, ls, amigos, adj, k, total):
+    return [x for x in range(k, total) if total - x + k >= n and all(map(lambda y: y != x and existeAresta(adj, x, y, amigos), ls[1:]))]
 
-def search(ls, n, arestas, tam, total, incluido):
-    if n == tam:
-        return valid(ls, n, arestas, incluido)
-    #elif total - ls[-1] - 1 + tam < n: # Esta condição não executa se as extensões estiverem bem feitas
-    #    return False
+def search(n, ls, adj, amigos, k, total):
+    if n == k:
+        return True
 
-    for x in extensions(ls, tam, total, n):
+    for x in extensions(n, ls, amigos, adj, k, total):
         ls.append(x)
-        incluido[x] = True
-        if search(ls, n, arestas, tam+1, total, incluido):
+        if search(n, ls, adj, amigos, k+1, total):
             return True
-        incluido[x] = False
         ls.pop()
 
     return False
 
-def cobertura(arestas):
-    vertices = list(set([i for tup in arestas for i in tup]))
+def amigos(conhecidos):
+    amigos = list(set([a for tup in conhecidos for a in tup]))
+    adj = {}
 
-    if len(arestas) == 0:
+    if len(conhecidos) == 0:
         return 0
 
-    N = len(vertices)
-    novasArestas = []
-    for a,b in arestas:
-        novasArestas.append( (vertices.index(a), vertices.index(b)) )
-        
-    incluido = [False for x in range(N)]
+    for x in amigos:
+        adj[x] = set()
+    for tup in conhecidos:
+        adj[tup[0]].add(tup[1])
+        adj[tup[1]].add(tup[0])
 
-    for i in range(1,N):
-        c = [-1]
-        if search(c, i, novasArestas, 0, N, incluido):
-            return i
-
-    return N
+    for n in range(len(amigos),2, -1):
+        ls = [-1]
+        if search(n, ls, adj, amigos, 0, len(amigos)):
+            return n
+    return 2
