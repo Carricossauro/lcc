a9b3892601c7063d43abdce91b0643331eed9770
Tiago Carriço <tiagocarrico01@gmail.com>
2021-05-08 12:58:16 +0100

    Update cobertura.py

--- Treino 4/cobertura.py
+++ Treino 4/cobertura.py
@@ -9,22 +9,24 @@ par de nós.
 '''
 
 # 10%
-def extensions(vertices, ls, k, total, n):
-    return [x for x in range(k, len(vertices)) if x not in ls and total - x + k >= n]
+def extensions(ls, k, total, n):
+    return [x for x in range(k, total) if x not in ls and total - x + k >= n]
 
-def valid(ls, n, arestas):
-    return all(map(lambda tup: tup[0] in ls or tup[1] in ls, arestas))
+def valid(ls, n, arestas, incluido):
+    return all(map(lambda tup: incluido[tup[0]] or incluido[tup[1]], arestas))
 
-def search(vertices, ls, n, arestas, tam, total):
+def search(ls, n, arestas, tam, total, incluido):
     if n == tam:
-        return valid(ls, n, arestas)
-    elif total - ls[-1] - 1 + tam < n: # Esta condição não executa se as extensões estiverem bem feitas
-        return False
+        return valid(ls, n, arestas, incluido)
+    #elif total - ls[-1] - 1 + tam < n: # Esta condição não executa se as extensões estiverem bem feitas
+    #    return False
 
-    for x in extensions(vertices, ls, tam, total, n):
+    for x in extensions(ls, tam, total, n):
         ls.append(x)
-        if search(vertices, ls, n, arestas, tam+1, total):
+        incluido[x] = True
+        if search(ls, n, arestas, tam+1, total, incluido):
             return True
+        incluido[x] = False
         ls.pop()
 
     return False
@@ -39,10 +41,12 @@ def cobertura(arestas):
     novasArestas = []
     for a,b in arestas:
         novasArestas.append( (vertices.index(a), vertices.index(b)) )
+        
+    incluido = [False for x in range(N)]
 
     for i in range(1,N):
         c = [-1]
-        if search(vertices, c, i, novasArestas, 0, N):
+        if search(c, i, novasArestas, 0, N, incluido):
             return i
 
     return N
