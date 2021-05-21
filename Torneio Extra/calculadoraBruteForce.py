3c9b2ad8aae8203ba8823322cd6da77985ee31ea
Tiago Carriço <tiagocarrico01@gmail.com>
2021-05-21 10:55:54 +0100

    Update calculadoraBruteForce.py

--- Torneio Extra/calculadoraBruteForce.py
+++ Torneio Extra/calculadoraBruteForce.py
@@ -1,3 +1,19 @@
+"""
+
+Dada uma calculadora que apenas tem disponível um conjunto fixo de operações e 
+cujo valor inicial é zero, implemente uma função que determina qual o número 
+mínimo de operações necessárias para atingir um determinado resultado. 
+Assuma que tal é sempre possível. 
+As operações disponíveis são representadas por uma sequência de strings, 
+onde cada string pode ser um dos caracteres '+', '-', '*' ou '/' 
+seguido de um número inteiro positivo (o segundo operando) 
+ou um único digito, que representa a operação de acrescentar um digito ao 
+número actualmente na calculadora. 
+Por exemplo, a string "/3" representa a operação de divisão inteira por 3 e 
+a string "4" a operação de acrescentar o digito 4 ao número actualmente 
+na calculadora (por exemplo, se o número actual é 3 ficará com o número 34).
+
+"""
 
 # 100%
 def search(ops, res, k, n, i):
