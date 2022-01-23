3a2004e72cfa2206f240e0817de37186740adbb4
Tiago Carriço <tiagocarrico27@gmail.com>
2022-01-23 19:35:10 +0000

    queries mongodb

--- TP2/code/queries_mongo.js
+++ TP2/code/queries_mongo.js
@@ -1,30 +1,64 @@
 // RE01 - Listar os clientes que frequentaram o estabelecimento
 
-db.Cliente.aggregate( [
-   {
-     $lookup: {
-         from: "Reserva",
-         localField: "_id",
-         foreignField: "Cliente",
-         as: "Reservas"
-     }
-   }
-] )
+db.Cliente.aggregate([
+    {
+        $lookup: {
+            from: "Reserva",
+            localField: "_id",
+            foreignField: "Cliente",
+            as: "Reservas",
+        },
+    },
+]);
 
 // RE02 – Listar os edifícios e respetivos alojamentos
 
-db.Edificio.aggregate( [
-   {
-     $lookup: {
-         from: "Alojamento",
-         localField: "_id",
-         foreignField: "Edificio",
-         as: "Alojamentos"
-     }
-   }
-] )
+db.Edificio.aggregate([
+    {
+        $lookup: {
+            from: "Alojamento",
+            localField: "_id",
+            foreignField: "Edificio",
+            as: "Alojamentos",
+        },
+    },
+    {
+        $project: {
+            "Rua/Morada": 1,
+            "Alojamentos.Numero": 1,
+            "Alojamentos.Numero_Quartos": 1,
+            "Alojamentos.Lotacao": 1,
+        },
+    },
+]).sort({ _id: 1 });
 
 // RE03 - Listar todas as reservas realizadas num certo alojamento
+
+db.Alojamento.aggregate([
+    { $match: {} },
+    { $unwind: "$Reservas" },
+    {
+        $lookup: {
+            from: "Reserva",
+            localField: "Reservas.Reserva",
+            foreignField: "_id",
+            as: "Reserva",
+        },
+    },
+    { $unwind: "$Reserva" },
+    {
+        $group: {
+            _id: "$_id",
+            Reservas: {
+                $push: {
+                    _id: "$Reserva._id",
+                    Data_Inicio: "$Reserva.Data_Inicio",
+                    Data_Fim: "$Reserva.Data_Fim",
+                },
+            },
+        },
+    },
+]);
 
 // RE04 – Listar todos os funcionários e respetivo responsável
 
@@ -32,15 +66,69 @@ db.Funcionario.find({}, { Nome: 1, Gerente: 1 });
 
 // RE05 – Calcular o número de vezes que cada alojamento foi reservado
 
+db.Alojamento.aggregate([
+    { $match: {} },
+    { $unwind: "$Reservas" },
+    {
+        $lookup: {
+            from: "Reserva",
+            localField: "Reservas.Reserva",
+            foreignField: "_id",
+            as: "Reserva",
+        },
+    },
+    { $unwind: "$Reserva" },
+    {
+        $group: {
+            _id: "$_id",
+            Reservas: { $sum: 1 },
+        },
+    },
+]);
+
 // RE06 – Calcular o número de pedidos que um funcionário processou
 
-db.Reserva.aggregate([{ $count: "Funcionario" }])
-db.Reserva.aggregate([{$group: {_id: "Funcionario", count: {$sum: 1} } }])
+db.Funcionario.aggregate([
+    {
+        $lookup: {
+            from: "Reserva",
+            localField: "_id",
+            foreignField: "Funcionario",
+            as: "Reservas",
+        },
+    },
+    { $unwind: "$Reservas" },
+    {
+        $group: {
+            _id: "$_id",
+            Nome: { $first: "$Nome" },
+            Reservas: { $sum: 1 },
+        },
+    },
+]).sort({ _id: 1 });
 
 // RE07 – Listar quantos quartos estão disponíveis até um certo preço
 
-db.Alojamento.find( { Preco_Base: { $lte: 100 } } )
+db.Alojamento.find({ Preco_Base: { $lte: 100 } });
 
 // RE08 - Listar os clientes por ordem descrescente de dinheiro gasto
 
-db.Reserva.aggregate([ {$unwind: "$Alojamentos"}, { $group: { _id: "$Cliente", Nome: {$sum: "Cliente_Dados.Nome"}, Total_Gasto: { $sum: "$Alojamentos.Preco" } } }]).sort({Total_Gasto: -1, "_id": 1})
+db.Reserva.aggregate([
+    {
+        $lookup: {
+            from: "Cliente",
+            localField: "Cliente",
+            foreignField: "_id",
+            as: "ClienteCol",
+        },
+    },
+    { $unwind: "$ClienteCol" },
+    { $unwind: "$Alojamentos" },
+    {
+        $group: {
+            _id: "$Cliente",
+            Nome: { $first: "$ClienteCol.Nome" },
+            Total_Gasto: { $sum: "$Alojamentos.Preco" },
+        },
+    },
+]).sort({ Total_Gasto: -1, _id: 1 });
