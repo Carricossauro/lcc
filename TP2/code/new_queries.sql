88b14002dffcc74544c8555c409c1110dd25cc00
Tiago Carriço <tiagocarrico27@gmail.com>
2022-01-26 15:27:37 +0000

    RE07 redone

--- TP2/code/new_queries.sql
+++ TP2/code/new_queries.sql
@@ -4,6 +4,8 @@ use AlojamentoLocal;
 select Alojamento, sum(Preco) from Reserva_Alojamento
 	group by Alojamento;
 
+-- RE07 – Listar os quartos que estão disponíveis até um certo preço
+SELECT Id_Alojamento, Preco_Base FROM Alojamento WHERE Preco_Base <= 100;
 
 -- RE08 - Listar os clientes por ordem descrescente de dinheiro gasto (Alterado para a nova estrutura)
 SELECT R.Cliente, C.Nome, SUM(RA.Preco) AS Total_Gasto FROM Reserva AS R
