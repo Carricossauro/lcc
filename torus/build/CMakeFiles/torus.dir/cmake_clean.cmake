f3415e30db7b709c17b9a691771ab5e8d6d75fa9
Tiago Carriço <tiagocarrico27@gmail.com>
2022-03-12 16:49:28 +0000

    Torus

---
+++ torus/build/CMakeFiles/torus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/torus.dir/main.cpp.o"
+  "torus.pdb"
+  "torus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/torus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
