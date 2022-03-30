ac78c79719107e7c4e9f641fa29accebe1de62d1
Inês Presa <a90355@alunos.uminho.pt>
2022-03-30 19:22:43 +0100

    navbar

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import NavBar from "./models/NavBar/NavBar";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NavBar />
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      color1: "#0fa3b1",
+      color2: "#d9e5d6",
+      color3: "#eddea4",
+      color4: "#f7a072",
+      color5: "#ff9b42",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
