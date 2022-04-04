69378468fe9af4a691529548e5c58f34dd821e4f
Inês Presa <a90355@alunos.uminho.pt>
2022-04-04 16:29:39 +0100

    NavBar mobile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,31 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import NavBar from "./models/NavBar/NavBar";
 
+const Index = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
+  return (
+    <>
+      <NavBar size={size} />
+    </>
+  );
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <NavBar />
-  </React.StrictMode>,
+  <>
+    <Index />
+  </>,
 
   document.getElementById("root")
 );
--- frontend/src/models/NavBar/NavBar.js
+++ frontend/src/models/NavBar/NavBar.js
@@ -4,56 +4,85 @@ import "./NavBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBrain, faBars } from "@fortawesome/free-solid-svg-icons";
 
-export default function NavBar() {
-    const [expanded, setExpanded] = useState(false);
+export default function NavBar(props) {
+  const [expanded, setExpanded] = useState(false);
 
-    const changeExpanded = () => {
-        setExpanded((state) => {
-            return !state;
-        });
-        console.log(expanded);
-    };
+  const changeExpanded = () => {
+    setExpanded((state) => {
+      return !state;
+    });
+  };
 
-    return (
-        <div className="fixed top-0 left-0 h-32 lg:h-16 w-screen bg-color5 flex flex-wrap text-white">
-            <div className="w-full lg:w-2/5 h-inherit flex items-center pl-2">
-                <div className="w-1/2 lg:w-full flex items-center">
-                    <FontAwesomeIcon icon={faBrain} className="icon" />
-                    <span className="ml-2 text-[30px] titleFont font-normal">
-                        Computational Mind
-                    </span>
-                </div>
-                <div className="lg:invisible flex justify-end items-center w-1/2 lg:w-0 cursor-pointer">
-                    <button onClick={changeExpanded}>
-                        <FontAwesomeIcon
-                            icon={faBars}
-                            className="icon-bars mr-2 hover:text-color1 duration-500"
-                        />
-                    </button>
-                </div>
-            </div>
-            <div className="w-full lg:w-3/5 h-inherit flex flex-wrap justify-center lg:justify-end items-center">
-                <div className="flex justify-center mx-3">
-                    <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
-                        ABOUT US
-                    </button>
-                </div>
-                <div className="flex justify-center mx-3">
-                    <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
-                        CONTACT
-                    </button>
-                </div>
-                <div className="flex justify-center mx-3">
-                    <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
-                        SIGN UP
-                    </button>
-                </div>
-                <div className="flex justify-center mx-3 mr-6">
-                    <button className="flex items-center justify-center px-5 h-9 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000">
-                        LOGIN
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="fixed top-0 left-0 h-16 w-screen bg-color5 flex flex-wrap text-white shadow-md">
+      <div className="w-full h-16 lg:w-2/5 h-inherit flex items-center pl-2">
+        <div className="w-2/3 lg:w-full flex items-center">
+          <FontAwesomeIcon icon={faBrain} className="icon" />
+          <span className="ml-2 text-[30px] titleFont font-normal">
+            Computational Mind
+          </span>
         </div>
-    );
+        <div className="lg:invisible flex justify-end items-center w-1/3 lg:w-0 cursor-pointer">
+          <button onClick={changeExpanded}>
+            <FontAwesomeIcon
+              icon={faBars}
+              className="icon-bars mr-2 hover:text-color1 duration-500"
+            />
+          </button>
+        </div>
+      </div>
+      {props.size >= 1024 && (
+        <div className="w-3/5 h-inherit flex flex-wrap justify-end items-center">
+          <div className="flex justify-center mx-3">
+            <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
+              ABOUT US
+            </button>
+          </div>
+          <div className="flex justify-center mx-3">
+            <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
+              CONTACT
+            </button>
+          </div>
+          <div className="flex justify-center mx-3">
+            <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
+              SIGN UP
+            </button>
+          </div>
+          <div className="flex justify-center mx-3 mr-6">
+            <button className="flex items-center justify-center px-5 h-9 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000">
+              LOGIN
+            </button>
+          </div>
+        </div>
+      )}
+      {props.size < 1024 && (
+        <div
+          className={`w-56 h-screen flex flex-col bg-color3 justify-start items-center absolute top-16 right-0 shadow-md duration-1000 ${
+            expanded ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <div className="flex justify-center my-3">
+            <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
+              ABOUT US
+            </button>
+          </div>
+          <div className="flex justify-center my-3">
+            <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
+              CONTACT
+            </button>
+          </div>
+          <div className="flex justify-center my-3">
+            <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
+              SIGN UP
+            </button>
+          </div>
+          <div className="flex justify-center my-3">
+            <button className="flex items-center justify-center h-9 w-32 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000">
+              LOGIN
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
