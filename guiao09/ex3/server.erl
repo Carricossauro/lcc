6bdd14a6ef3fa3808b86f713683f0d64f62812f4
Tiago Carriço <tiagocarrico27@gmail.com>
2022-05-06 10:10:07 +0100

    erlang chat v15.0

--- guiao09/ex3/server.erl
+++ guiao09/ex3/server.erl
@@ -13,21 +13,28 @@ server(Port) ->
     end.
 
 acceptor(LSock, Rooms) ->
-    {ok, Sock} = gen_tcp:accept(LSock),
-    spawn(fun() -> acceptor(LSock, Rooms) end),
-    Rooms ! {login, self()},
-    user(Sock, Rooms, "main", "Anonymous").
+    Res = gen_tcp:accept(LSock),
+    case Res of
+        {ok, Sock} ->
+            spawn(fun() -> acceptor(LSock, Rooms) end),
+            Rooms ! {login, self()},
+            user(Sock, Rooms, "main", "Anonymous");
+        {error, closed} ->
+            ok
+    end.
 
 rooms(Pids) ->
     receive
         {login, Pid} ->
             NewPids = maps:put(Pid, {"Anonymous", "main"}, Pids),
-            io:format("User entered main~n", []), 
+            io:format("User entered main~n", []),
+            sendMessage(Pid, "Erlang Chat v2.0 (\"main\")\n"),
             rooms(NewPids);
         {join, Room, Pid} ->
             {User, CurrentRoom} = maps:get(Pid, Pids),
-            io:format("~p entered ~p (left ~p)~n", [User, Room, CurrentRoom]),
             NewPids = maps:put(Pid, {User, Room}, Pids),
+            io:format("~p entered ~p (left ~p)~n", [User, Room, CurrentRoom]),
+            sendMessage(Pid, "Joined room " ++ Room ++ ".\n"),
             rooms(NewPids);
         {changeUsername, Name, Pid} ->
             {User, CurrentRoom} = maps:get(Pid, Pids),
@@ -41,14 +48,17 @@ rooms(Pids) ->
                             handleError("User " ++ Name ++ " already exists", Pid),
                             rooms(Pids);
                         false ->
-                            io:format("~p changed username to ~p~n", [User, Name]),
                             NewPids = maps:put(Pid, {Name, CurrentRoom}, Pids),
+                            io:format("~p changed username to ~p~n", [User, Name]),
+                            sendMessage(Pid, "Changed username to " ++ Name ++ ".\n"),
                             rooms(NewPids)
                     end
             end;
         {message, Data, Pid} ->
             {User, Room} = maps:get(Pid, Pids),
-            [UPid ! {broadcast, User ++ " > " ++ Data} || {UPid, {UUser, URoom}} <- maps:to_list(Pids), (URoom == Room) and (User /= UUser)],
+            Message = User ++ " (" ++ Room ++ ") > " ++ Data,
+            [UPid ! {broadcast, Message} || {UPid, {UUser, URoom}} <- maps:to_list(Pids), (URoom == Room) and (User /= UUser)],
+            io:fwrite(Message),
             rooms(Pids);
         {logout, Pid} ->
             {User, Room} = maps:get(Pid, Pids),
@@ -57,9 +67,11 @@ rooms(Pids) ->
             rooms(NewPids)
     end.
 
+sendMessage(Pid, Message) -> Pid ! {broadcast, Message}.
+
 handleError(Error, Pid) ->
     io:format("Error: ~p~n", [Error]),
-    Pid ! {broadcast, "Error: " ++ Error ++ "\n"}.
+    sendMessage(Pid, "Error: " ++ Error ++ "\n").
 
 user(Sock, Rooms, CurrentRoom, User) ->
     receive
@@ -81,7 +93,10 @@ user(Sock, Rooms, CurrentRoom, User) ->
                     Rooms ! {message, RealContent, self()},
                     user(Sock, Rooms, CurrentRoom, User);
                 _ ->
-                    gen_tcp:send(Sock, "Error: Incorrect syntax\n"),
+                    gen_tcp:send(Sock, "Error: Incorrect syntax. Use:\n"),
+                    gen_tcp:send(Sock, "join [room name]\n"),
+                    gen_tcp:send(Sock, "changeUser [new username]\n"),
+                    gen_tcp:send(Sock, "msg [message]\n"),
                     user(Sock, Rooms, CurrentRoom, User)
             end;
         {tcp_closed, _} ->
