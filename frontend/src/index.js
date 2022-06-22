174af9dc4b086c383849fb42b8337837858d4ad2
Tiago Carriço <tiagocarrico27@gmail.com>
2022-06-22 19:11:32 +0100

    about us page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,88 +8,94 @@ import Login from "./models/Login/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Player from "./models/Player/Player";
 import Author from "./models/Author/Author";
+import About from "./models/About/About";
 import { useCookies } from "react-cookie";
 
 const Index = () => {
-  const [size, setSize] = useState(window.innerWidth);
-  const [loginPopUp, setLoginPopUp] = useState(false);
-  const [showNavBar, setShowNavBar] = useState(true);
-  const [isAuthor, setIsAuthor] = useState(false);
+    const [size, setSize] = useState(window.innerWidth);
+    const [loginPopUp, setLoginPopUp] = useState(false);
+    const [showNavBar, setShowNavBar] = useState(true);
+    const [isAuthor, setIsAuthor] = useState(false);
 
-  const [cookies, setCookie] = useCookies(["user"]);
+    const [cookies, setCookie] = useCookies(["user"]);
 
-  const checkSize = () => {
-    setSize(window.innerWidth);
-  };
-
-  const changePopUpState = () => {
-    setLoginPopUp((state) => {
-      return !state;
-    });
-  };
-  useEffect(() => {
-    window.addEventListener("resize", checkSize);
-    return () => {
-      window.removeEventListener("resize", checkSize);
+    const checkSize = () => {
+        setSize(window.innerWidth);
     };
-  }, []);
 
-  return (
-    <div>
-      <Router>
-        {showNavBar && (
-          <NavBar
-            size={size}
-            setPopUp={changePopUpState}
-            setCookie={setCookie}
-            cookies={cookies}
-          />
-        )}
-        <Routes>
-          <Route
-            path="/Player/:playerPath"
-            element={<Player setShowNavBar={setShowNavBar} size={size} />}
-          ></Route>
-          <Route
-            path="/Author/:authorPath"
-            element={<Author setShowNavBar={setShowNavBar} size={size} />}
-          ></Route>
-          <Route
-            path="/Login"
-            element={
-              <Login
-                setShowNavBar={setShowNavBar}
-                size={size}
-                setIsAuthor={setIsAuthor}
-                isAuthor={isAuthor}
-                setCookie={setCookie}
-                cookies={cookies}
-              />
-            }
-          ></Route>
-          <Route
-            path="/SignUp"
-            element={
-              <SignUp
-                setShowNavBar={setShowNavBar}
-                size={size}
-                setIsAuthor={setIsAuthor}
-                isAuthor={isAuthor}
-                setCookie={setCookie}
-                cookies={cookies}
-              />
-            }
-          ></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+    const changePopUpState = () => {
+        setLoginPopUp((state) => {
+            return !state;
+        });
+    };
+    useEffect(() => {
+        window.addEventListener("resize", checkSize);
+        return () => {
+            window.removeEventListener("resize", checkSize);
+        };
+    }, []);
+
+    return (
+        <div>
+            <Router>
+                {showNavBar && (
+                    <NavBar
+                        size={size}
+                        setPopUp={changePopUpState}
+                        setCookie={setCookie}
+                        cookies={cookies}
+                    />
+                )}
+                <Routes>
+                    <Route
+                        path="/Player/:playerPath"
+                        element={
+                            <Player setShowNavBar={setShowNavBar} size={size} />
+                        }
+                    ></Route>
+                    <Route
+                        path="/Author/:authorPath"
+                        element={
+                            <Author setShowNavBar={setShowNavBar} size={size} />
+                        }
+                    ></Route>
+                    <Route
+                        path="/Login"
+                        element={
+                            <Login
+                                setShowNavBar={setShowNavBar}
+                                size={size}
+                                setIsAuthor={setIsAuthor}
+                                isAuthor={isAuthor}
+                                setCookie={setCookie}
+                                cookies={cookies}
+                            />
+                        }
+                    ></Route>
+                    <Route
+                        path="/SignUp"
+                        element={
+                            <SignUp
+                                setShowNavBar={setShowNavBar}
+                                size={size}
+                                setIsAuthor={setIsAuthor}
+                                isAuthor={isAuthor}
+                                setCookie={setCookie}
+                                cookies={cookies}
+                            />
+                        }
+                    ></Route>
+                    <Route path="/About" element={<About />}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 };
 
 ReactDOM.render(
-  <>
-    <Index />
-  </>,
+    <>
+        <Index />
+    </>,
 
-  document.getElementById("root")
+    document.getElementById("root")
 );
--- frontend/src/models/NavBar/NavBar.js
+++ frontend/src/models/NavBar/NavBar.js
@@ -5,104 +5,107 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBrain, faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar(props) {
-  const [expanded, setExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
-  useEffect(() => {
-    if (props.size >= 1024) setExpanded(false);
-  }, [props.size]);
+    useEffect(() => {
+        if (props.size >= 1024) setExpanded(false);
+    }, [props.size]);
 
-  const changeExpanded = () => {
-    setExpanded((state) => {
-      return !state;
-    });
-  };
+    const changeExpanded = () => {
+        setExpanded((state) => {
+            return !state;
+        });
+    };
 
-  const redirect = (page) => {
-    window.location.href = page;
-  };
+    const redirect = (page) => {
+        window.location.href = page;
+    };
 
-  return (
-    <div className="fixed top-0 left-0 h-16 w-screen bg-color5 flex flex-wrap text-white shadow-md">
-      <div className="w-full h-16 lg:w-2/5 h-inherit flex items-center pl-2">
-        <div className="w-2/3 lg:w-full flex items-center">
-          <FontAwesomeIcon icon={faBrain} className="icon" />
-          <span className="ml-2 text-[30px] titleFont font-normal">
-            Computational Mind
-          </span>
+    return (
+        <div className="fixed top-0 left-0 h-16 w-screen bg-color5 flex flex-wrap text-white shadow-md">
+            <div className="w-full h-16 lg:w-2/5 h-inherit flex items-center pl-2">
+                <div className="w-2/3 lg:w-full flex items-center">
+                    <FontAwesomeIcon icon={faBrain} className="icon" />
+                    <span className="ml-2 text-[30px] titleFont font-normal">
+                        Computational Mind
+                    </span>
+                </div>
+                <div className="lg:invisible flex justify-end items-center w-1/3 lg:w-0 cursor-pointer">
+                    <button onClick={changeExpanded}>
+                        <FontAwesomeIcon
+                            icon={faBars}
+                            className="icon-bars mr-2 hover:text-color1 duration-500"
+                        />
+                    </button>
+                </div>
+            </div>
+            {props.size >= 1024 && (
+                <div className="w-3/5 h-inherit flex flex-wrap justify-end items-center">
+                    <div className="flex justify-center mx-3">
+                        <button
+                            className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000"
+                            onClick={() => redirect("/About")}
+                        >
+                            ABOUT US
+                        </button>
+                    </div>
+                    <div className="flex justify-center mx-3">
+                        <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
+                            CONTACT
+                        </button>
+                    </div>
+                    <div className="flex justify-center mx-3">
+                        <button
+                            className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000"
+                            onClick={() => redirect("/SignUp")}
+                        >
+                            SIGN UP
+                        </button>
+                    </div>
+                    <div className="flex justify-center mx-3 mr-6">
+                        <button
+                            className="flex items-center justify-center w-24 h-9 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000"
+                            onClick={() => redirect("/Login")}
+                        >
+                            LOGIN
+                        </button>
+                    </div>
+                </div>
+            )}
+            {props.size < 1024 && (
+                <div
+                    className={`w-56 h-screen flex flex-col bg-color3 justify-start items-center absolute top-16 right-0 shadow-md duration-1000 ${
+                        expanded ? "translate-x-0" : "translate-x-full"
+                    }`}
+                >
+                    <div className="flex justify-center my-3">
+                        <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
+                            ABOUT US
+                        </button>
+                    </div>
+                    <div className="flex justify-center my-3">
+                        <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
+                            CONTACT
+                        </button>
+                    </div>
+                    <div className="flex justify-center my-3">
+                        <button
+                            className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000"
+                            onClick={() => redirect("/SignUp")}
+                        >
+                            SIGN UP
+                        </button>
+                    </div>
+                    <div className="flex justify-center my-3">
+                        <button
+                            className="flex items-center justify-center h-9 w-32 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000"
+                            onClick={() => redirect("/Login")}
+                        >
+                            LOGIN
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-        <div className="lg:invisible flex justify-end items-center w-1/3 lg:w-0 cursor-pointer">
-          <button onClick={changeExpanded}>
-            <FontAwesomeIcon
-              icon={faBars}
-              className="icon-bars mr-2 hover:text-color1 duration-500"
-            />
-          </button>
-        </div>
-      </div>
-      {props.size >= 1024 && (
-        <div className="w-3/5 h-inherit flex flex-wrap justify-end items-center">
-          <div className="flex justify-center mx-3">
-            <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
-              ABOUT US
-            </button>
-          </div>
-          <div className="flex justify-center mx-3">
-            <button className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000">
-              CONTACT
-            </button>
-          </div>
-          <div className="flex justify-center mx-3">
-            <button
-              className="flex items-center justify-center px-5 h-9 hover:bg-color1 rounded cursor-pointer duration-1000"
-              onClick={() => redirect("/SignUp")}
-            >
-              SIGN UP
-            </button>
-          </div>
-          <div className="flex justify-center mx-3 mr-6">
-            <button
-              className="flex items-center justify-center w-24 h-9 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000"
-              onClick={() => redirect("/Login")}
-            >
-              LOGIN
-            </button>
-          </div>
-        </div>
-      )}
-      {props.size < 1024 && (
-        <div
-          className={`w-56 h-screen flex flex-col bg-color3 justify-start items-center absolute top-16 right-0 shadow-md duration-1000 ${
-            expanded ? "translate-x-0" : "translate-x-full"
-          }`}
-        >
-          <div className="flex justify-center my-3">
-            <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
-              ABOUT US
-            </button>
-          </div>
-          <div className="flex justify-center my-3">
-            <button className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000">
-              CONTACT
-            </button>
-          </div>
-          <div className="flex justify-center my-3">
-            <button
-              className="flex items-center justify-center h-9 w-32 hover:bg-color1 rounded cursor-pointer duration-1000"
-              onClick={() => redirect("/SignUp")}
-            >
-              SIGN UP
-            </button>
-          </div>
-          <div className="flex justify-center my-3">
-            <button
-              className="flex items-center justify-center h-9 w-32 bg-color1 rounded cursor-pointer hover:text-color4 hover:font-bold duration-1000"
-              onClick={() => redirect("/Login")}
-            >
-              LOGIN
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
