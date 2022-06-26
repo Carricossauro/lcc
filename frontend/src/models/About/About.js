b32ce6ae55116c4a5c68cb425e74cc89763bd740
Inês Presa <a90355@alunos.uminho.pt>
2022-06-26 22:47:50 +0100

    Update Html & Css

--- frontend/src/models/About/About.js
+++ frontend/src/models/About/About.js
@@ -7,7 +7,7 @@ export default function About() {
     <div className="mt-16 flex items-center flex-col">
       <class className="h-[40vh] sm:h-[50vh] w-screen text-green about-image"></class>
       <h1 className="text-6xl mt-4">About Us</h1>
-      <div className="px-10 sm:w-[50%] sm.:px-0 mt-6 text-xl text-justify">
+      <div className="px-10 sm:w-[50%] sm:px-0 mt-6 text-xl text-justify">
         Created in 2022 by a group of students from the Degree in Computer
         Science at the University of Minho for the course unit project.
         ComputacionalMind was devised by teachers Pedro Rangel Henriques and
@@ -15,7 +15,7 @@ export default function About() {
         everyone, but especially to younger people, in order to prepare new
         generations for an increasingly computerised world.
       </div>
-      <div className="mt-6 flex justify-center items-center gap-x-10">
+      <div className="my-6 flex justify-center items-center gap-x-10">
         <img src={ecum} className="aspect-auto w-[75px]" />
         <img src={um} className="aspect-auto w-[75px]" />
         <img src={eeum} className="aspect-auto w-[75px]" />
