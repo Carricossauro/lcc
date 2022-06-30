6d79569d2043ddf7f41fd035f5827b9ac0aaa19d
Inês Presa <a90355@alunos.uminho.pt>
2022-06-30 04:10:07 +0100

    wip

--- frontend/src/models/Landing/Landing.js
+++ frontend/src/models/Landing/Landing.js
@@ -1,29 +1,36 @@
 import "./Landing.css";
+import brain from "../../assets/brain.svg";
 
 export default function Landing() {
   return (
     <div className="pt-16 h-screen">
-      <header className="image bg-cover h-full">
+      <header className=" bg-gray-50 bg-cover h-full">
         <div className="content px-8 py-2">
           <div className="body mt-20 mx-8">
-            <div className="md:flex items-center justify-between">
-              <div className=" mt-40 w-full md:w-1/2 mr-auto">
-                <h1 className="text-4xl sm:text-5xl font-bold text-white tracking-wide mb-2">
+            <div className="md:flex md:flex-row flex-col items-center">
+              <div className=" w-full md:min-w-[120px]">
+                <h1 className="text-4xl sm:text-5xl font-bold text-color1 tracking-wide mb-2">
                   Computational Mind
                 </h1>
-                <h2 className="text-2xl sm:text-3xl font-bold text-white tracking-wide">
-                  Welcome <span class="text-gray-800"> Aboard</span>
+                <h2 className="text-2xl sm:text-3xl font-bold text-color4 tracking-wide">
+                  Welcome <span className="text-color1"> Aboard</span>
                 </h2>
-                <p className="text-gray-300  sm:text-lg">
+                <p className="text-color5  sm:text-lg">
                   Play or create problems/chalenges to improve Computational
                   Thinking.
                 </p>
-                <span className="text-white sm:text-lg flex flex-col">
+                <span className="text-color4 sm:text-lg flex flex-col">
                   Don't have an Account?
-                  <a href="/SignUp" class="text-gray-900 sm:text-xl font-bold">
+                  <a
+                    href="/SignUp"
+                    className="text-color5 sm:text-xl font-bold"
+                  >
                     Register here.
                   </a>
                 </span>
+              </div>
+              <div class="flex items-center w-full justify-center p-0 md:p-6 mt-10 md:mt-0">
+                <img src={brain} alt="brain" className="max-w-[420px]" />
               </div>
             </div>
           </div>
--- frontend/src/models/Login/Login.js
+++ frontend/src/models/Login/Login.js
@@ -7,173 +7,167 @@ import image from "../../assets/imagem_conhecimento.jpg";
 import { login } from "./API_login";
 
 export default function PlayerLogin({
-    size,
-    setShowNavBar,
-    setIsAuthor,
-    isAuthor,
-    cookies,
-    setCookie,
-    removeCookie,
+  size,
+  setShowNavBar,
+  setIsAuthor,
+  isAuthor,
+  cookies,
+  setCookie,
+  removeCookie,
 }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    useEffect(() => {
-        setShowNavBar(false);
-    }, []);
+  useEffect(() => {
+    setShowNavBar(false);
+  }, []);
 
-    const redirect = (page) => {
-        window.location.href = page;
-    };
+  const redirect = (page) => {
+    window.location.href = page;
+  };
 
-    const backToMain = (e) => {
-        e.preventDefault();
-        redirect("/");
-    };
+  const backToMain = (e) => {
+    e.preventDefault();
+    redirect("/");
+  };
 
-    async function verifyLogin(e) {
-        e.preventDefault();
-        setError("");
+  async function verifyLogin(e) {
+    e.preventDefault();
+    setError("");
 
-        try {
-            const response = await login(username, password, isAuthor);
+    try {
+      const response = await login(username, password, isAuthor);
 
-            setCookie("access_token", response.access, { path: "/" });
-            setCookie("refresh_token", response.refresh, { path: "/" });
-            setCookie("username", username, { path: "/" });
+      setCookie("access_token", response.access, { path: "/" });
+      setCookie("refresh_token", response.refresh, { path: "/" });
+      setCookie("username", username, { path: "/" });
 
-            redirect(`/${isAuthor ? "Author" : "Player"}/Main`);
-        } catch (e) {
-            console.log(e);
-            removeCookie("access_token");
-            removeCookie("refresh_token");
-            removeCookie("username");
-            if (e === "type") {
-                setError(
-                    `This user is not ${isAuthor ? "an author" : "a player"}.`
-                );
-            } else setError("Invalid username or password.");
-        }
+      redirect(`/${isAuthor ? "Author" : "Player"}/Main`);
+    } catch (e) {
+      console.log(e);
+      removeCookie("access_token");
+      removeCookie("refresh_token");
+      removeCookie("username");
+      if (e === "type") {
+        setError(`This user is not ${isAuthor ? "an author" : "a player"}.`);
+      } else setError("Invalid username or password.");
     }
+  }
 
-    return (
-        <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-t from-white to-color2">
-            <div className="bg-white min-h-[450px] h-[90%] w-[90%] lg:h-[700px] lg:w-[1200px] rounded-lg drop-shadow-xl flex flex-col">
-                <div className="w-full h-[40px] bg-split-color2-white flex flex-row ">
-                    <div
-                        className={`w-1/2 flex justify-center items-center text-xl ${
-                            !isAuthor
-                                ? "bg-white rounded-tr-2xl"
-                                : "bg-color2 rounded-br-2xl cursor-pointer text-slate-500 hover:text-slate-900 duration-200"
-                        }`}
-                        onClick={() => {
-                            setError("");
-                            setIsAuthor(false);
-                        }}
-                    >
-                        {" "}
-                        Player
-                    </div>
-                    <div
-                        className={`w-1/2 flex justify-center items-center text-xl ${
-                            !isAuthor
-                                ? "bg-color2 rounded-bl-2xl cursor-pointer text-slate-500 hover:text-slate-900 duration-200"
-                                : "bg-white rounded-tl-2xl"
-                        }`}
-                        onClick={() => {
-                            setError("");
-                            setIsAuthor(true);
-                        }}
-                    >
-                        {" "}
-                        Author
-                    </div>
-                </div>
-                <div className="w-full flex flex-row h-full">
-                    {size >= 1024 && (
-                        <div className="w-1/2 h-full flex items-center justify-center">
-                            <img
-                                src={image}
-                                alt="image"
-                                className="aspect-auto w-[500px] ml-10"
-                            />
-                        </div>
-                    )}
-                    <form className="w-full lg:w-1/2 flex justify-center flex-col items-center">
-                        <button
-                            className="flex items-center hover:text-color1 duration-500 mb-10"
-                            onClick={backToMain}
-                        >
-                            <FontAwesomeIcon
-                                icon={faBrain}
-                                className="text-[30px] sm:text-[40px]"
-                            />
-                            <span className="ml-2 text-xl text-[28px]">
-                                Computational Mind
-                            </span>
-                        </button>
-                        {/* <div className="mb-8 text-3xl">
+  return (
+    <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-t from-white to-color2">
+      <div className="bg-white min-h-[450px] h-[90%] w-[90%] lg:h-[700px] lg:w-[1200px] rounded-lg drop-shadow-xl flex flex-col">
+        <div className="w-full h-[40px] bg-split-color2-white flex flex-row ">
+          <div
+            className={`w-1/2 flex justify-center items-center text-xl ${
+              !isAuthor
+                ? "bg-white rounded-tr-2xl"
+                : "bg-color2 rounded-br-2xl cursor-pointer text-slate-500 hover:text-slate-900 duration-200"
+            }`}
+            onClick={() => {
+              setError("");
+              setIsAuthor(false);
+            }}
+          >
+            {" "}
+            Player
+          </div>
+          <div
+            className={`w-1/2 flex justify-center items-center text-xl ${
+              !isAuthor
+                ? "bg-color2 rounded-bl-2xl cursor-pointer text-slate-500 hover:text-slate-900 duration-200"
+                : "bg-white rounded-tl-2xl"
+            }`}
+            onClick={() => {
+              setError("");
+              setIsAuthor(true);
+            }}
+          >
+            {" "}
+            Author
+          </div>
+        </div>
+        <div className="w-full flex flex-row h-full">
+          {size >= 1024 && (
+            <div className="w-1/2 h-full flex items-center justify-center">
+              <img
+                src={image}
+                alt="image"
+                className="aspect-auto w-[500px] ml-10"
+              />
+            </div>
+          )}
+          <form className="w-full lg:w-1/2 flex justify-center flex-col items-center">
+            <button
+              className="flex items-center hover:text-color1 duration-500 mb-10"
+              onClick={backToMain}
+            >
+              <FontAwesomeIcon
+                icon={faBrain}
+                className="text-[30px] sm:text-[40px]"
+              />
+              <span className="ml-2 text-xl text-[28px]">
+                Computational Mind
+              </span>
+            </button>
+            {/* <div className="mb-8 text-3xl">
                             {isAuthor ? "Author" : "Player"} Login
                         </div> */}
-                        <div className="flex items-center px-3 h-12 w-72 sm:w-96 bg-stone-200  rounded-3xl mb-3">
-                            <FontAwesomeIcon
-                                icon={faUser}
-                                className="text-2xl  text-neutral-500 ml-2.5"
-                            />
-                            <input
-                                className="outline-0 ml-3 bg-inherit w-full"
-                                type="text"
-                                id="username"
-                                name="username"
-                                value={username}
-                                placeholder="username"
-                                onChange={(e) => setUsername(e.target.value)}
-                            ></input>
-                        </div>
-                        <div
-                            className={`flex items-center px-3 h-12 w-72 sm:w-96 bg-stone-200  rounded-3xl ${
-                                error != "" ? "mb-1" : "mb-5"
-                            }`}
-                        >
-                            <FontAwesomeIcon
-                                icon={faLock}
-                                className="text-2xl  text-neutral-500 ml-2.5"
-                            />
-                            <input
-                                className="outline-0 ml-3 bg-inherit w-full"
-                                type="password"
-                                id="password"
-                                name="password"
-                                value={password}
-                                placeholder="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                            ></input>
-                        </div>
-                        {error != "" && (
-                            <div className="text-red-500 italic">{error}</div>
-                        )}
-                        <button
-                            className="flex items-center justify-center h-12 w-72 sm:w-96 rounded-3xl cursor-pointer bg-color1 text-lg"
-                            onClick={(e) => verifyLogin(e)}
-                        >
-                            LOGIN
-                        </button>
-                        <div className="w-full text-center my-5">
-                            ______________________
-                        </div>
-                        <div className="w-full text-center">
-                            Don't have an account?
-                        </div>
-                        <div
-                            className="w-full text-center mt-5 cursor-pointer hover:text-color1 hover:underline duration-100 font-bold"
-                            onClick={() => redirect("/SignUp")}
-                        >
-                            Create an account now!
-                        </div>
-                    </form>
-                </div>
+            <div className="flex items-center px-3 h-12 w-72 sm:w-96 bg-stone-200  rounded-3xl mb-3">
+              <FontAwesomeIcon
+                icon={faUser}
+                className="text-2xl  text-neutral-500 ml-2.5"
+              />
+              <input
+                className="outline-0 ml-3 bg-inherit w-full"
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                placeholder="username"
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
             </div>
+            <div
+              className={`flex items-center px-3 h-12 w-72 sm:w-96 bg-stone-200  rounded-3xl ${
+                error != "" ? "mb-1" : "mb-5"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faLock}
+                className="text-2xl  text-neutral-500 ml-2.5"
+              />
+              <input
+                className="outline-0 ml-3 bg-inherit w-full"
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            {error != "" && <div className="text-red-500 italic">{error}</div>}
+            <button
+              className="flex items-center justify-center h-12 w-72 sm:w-96 rounded-3xl cursor-pointer bg-color1 text-lg"
+              onClick={(e) => verifyLogin(e)}
+            >
+              LOGIN
+            </button>
+            <div className="w-full text-center my-5">
+              ______________________
+            </div>
+            <div className="w-full text-center">Don't have an account?</div>
+            <div
+              className="w-full text-center mt-5 cursor-pointer hover:text-color1 hover:underline duration-100 font-bold"
+              onClick={() => redirect("/SignUp")}
+            >
+              Create an account now!
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
