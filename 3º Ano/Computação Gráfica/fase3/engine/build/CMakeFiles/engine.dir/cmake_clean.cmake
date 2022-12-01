19cce32e1b591fba67e433012c978eadcca037a4
Tiago Carriço <tiagocarrico27@gmail.com>
2022-12-01 14:26:24 +0000

    3º Ano

---
+++ 3º Ano/Computação Gráfica/fase3/engine/build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/main.cpp.o"
+  "CMakeFiles/engine.dir/tinyxml2.cpp.o"
+  "CMakeFiles/engine.dir/structs.cpp.o"
+  "engine.pdb"
+  "engine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3º Ano/Computação Gráfica/fase4/engine/build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/carricossauro/Documents/LCC 3º ano/Projeto-CG/fase4/engine/main.cpp" "/home/carricossauro/Documents/LCC 3º ano/Projeto-CG/fase4/engine/build/CMakeFiles/engine.dir/main.cpp.o"
+  "/home/carricossauro/Documents/LCC 3º ano/Projeto-CG/fase4/engine/structs.cpp" "/home/carricossauro/Documents/LCC 3º ano/Projeto-CG/fase4/engine/build/CMakeFiles/engine.dir/structs.cpp.o"
+  "/home/carricossauro/Documents/LCC 3º ano/Projeto-CG/fase4/engine/tinyxml2.cpp" "/home/carricossauro/Documents/LCC 3º ano/Projeto-CG/fase4/engine/build/CMakeFiles/engine.dir/tinyxml2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/IL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
